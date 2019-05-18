1e757c4e37653e9f908eaf69be492efdd5c836f6
KrisShiro <49657132+KrisShiro@users.noreply.github.com>
2019-05-18 19:48:50 +0300

    fixes STATIC_URL

--- pererabotka/settings.py
+++ pererabotka/settings.py
@@ -25,7 +25,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['pererabo.tk']
 
-STATIC_URL = '/static'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
@@ -118,5 +118,3 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATIC_URL = '/static/'
