bffb8c391dd4b9aa248d967b0020181b75e753ab
KrisShiro <49657132+KrisShiro@users.noreply.github.com>
2019-05-18 20:08:27 +0300

    Comment

--- map/models.py
+++ map/models.py
@@ -10,11 +10,12 @@ class Place(models.Model):
     coordinate_y = models.FloatField()
     type_mask = models.IntegerField()
 
+    #Маска
     PLAST = 1
     PAPER = 2
     GLASS = 4
     ACCUM = 8
-    TYPES = [PLAST, PAPER, GLASS, ACCUM]
+    TYPES = [PLAST, PAPER, GLASS, ACCUM]   
 
     def __str__(self):
         return self.address
